79753cb616bb4ef84d9240a482499959f713a03a
Andreas Krohn <andreas@dopter.se>
2014-06-03 10:52:32 +0200

    Initial commit

---
+++ kolada.js
@@ -0,0 +1,81 @@
+// Configuration
+municipality = 1283;
+municipality_name = "Helsingborgs stad"
+metrics = [          
+  {"kpi": "N01951", "title":"Invånare totalt, antal ", "description":"Antal invånare totalt den 31/12. Källa: SCB."},
+  {"kpi": "N01804", "title":"Födda, antal ", "description":"Antal födda under året. Källa: SCB."},
+  {"kpi": "N11008", "title":"Kostnad förskola, kr/inskrivet barn", "description":"Bruttokostnad minus interna intäkter och försäljning till andra kommuner och landsting för förskola, dividerat med genomsnittligt antal inskrivna barn i förskola vid mätning 15/10 föregående och innevarande år. Avser samtlig regi. Källa: SCB och Skolverket."},
+  {"kpi": "N21010", "title":"Kostnad hemtjänst äldreomsorg, kr/brukare", "description":"Bruttokostnad minus interna intäkter och försäljning till andra kommuner och landsting för hemtjänst äldreomsorg, dividerat med antal personer 65+ som den 1/10 var beviljade hemtjänst i ordinärt boende. Fr.o.m. 2007 hämtas antalet hemtjänsttagare från Socialstyrelsens individstatistik. T.o.m. 2006 från Socialstyrelsens mängdstatistik. Avser samtlig regi. Källa: SCB och Socialstyrelsen."},
+  {"kpi": "N09100", "title":"Kostnad kulturverksamhet, kr/inv", "description":"Bruttokostnad minus interna intäkter och försäljning till andra kommuner och landsting inom kulturområdet (stöd till föreningar, bibliotek, musikskola etc) per invånare den 31/12. Avser stöd till studieorganisationer, allmän kulturverksamhet, bibliotek samt musik- och kulturskola. Avser samtlig regi. Källa: SCB:s Räkenskapssammandrag."},
+  {"kpi": "N07005", "title":"Kostnad parker, kr/inv", "description":"Bruttokostnad minus interna intäkter och försäljning till andra kommuner och landsting för parker, dividerat med antal invånare totalt 31/12. Avser kommunal parkverksamhet som parker, lekplatser, offentliga toaletter, naturområden etc. Här redovisas även kostnader för naturreservat om det inte har en mycket uttalad naturvårdsprofil, då de redovisas under miljö, hälsa och hållbar utveckling. Avser samtlig regi. Källa: SCB:s Räkenskapssammandrag."},
+  {"kpi": "N00951", "title":"Lönegap median kvinnor - median män kommunsektorn, kr", "description":"Lönegap median kvinnor - median män kommunsektorn, kronor. Avser invånare som är kommunalt anställda (d.v.s. anställda av den kommun de bor i eller av någon annan kommun). Anställda i kommunala bolag och kommunalförbund ingår inte här. Källa: SCB."},
+  {"kpi": "N00941", "title":"Nyregistrerade företag kommun, antal/1000 invånare", "description":"Antalet nyregistrerade företag i kommunen dividerat med antal invånare 31/12. Avser aktiebolag, enskilda näringsidkare, handelsbolag och kommanditbolag. Källa: Bolagsverket och SCB."},
+  {"kpi": "U00414", "title":"U. Andel som upplever ett gott bemötande vid kontakt med kommunen, (%)", "description":"Andel (%) av medborgarna som uppfattar att de får ett gott bemötande när de kontaktar kommunen kring en enkel fråga. Baseras på en subjektiv bedömning av frågeställaren utifrån tre nivåer; god, medelgod och dålig. Procentsatsen gäller enbart för gott bemötande. Källa: Undersökning av externt företag"},
+  {"kpi": "U07801", "title":"U. Insamlat hushållsavfall kommun, kg/inv", "description":"Antal kg hushållsavfall per invånare och år i kommunen. I hushållsavfall ingår kärl- och säckavfall, grovavfall inklusive trädgårdsavfall, farligt avfall, jämförligt avfall från bland annat affärer, kontor, industrier och restauranger, samt den del av hushållsavfallet som omfattas av producentansvar (förpackningar, tidningar, elavfall, batterier etc.), även om det inte faller under kommunalt renhållningsansvar. Invånarantalet justeras med hänsyn till fritidshus, gästnätter och in-/utpendling. I utpräglade fritidshuskommuner eller kommuner med omfattande turistverksamhet genereras betydligt mer hushållsavfall än vad de permanentboende ger upphov till. Även i kommuner med stort inpendlingsöverskott genereras mycket hushållsavfall via arbetsplatserna. Genom att använda ett justerat invånarantal speglas mängden avfall per person på ett mer korrekt sätt för den egna kommunen, vilket underlättar jämförelser med andra kommuner. Källa: Avfall Sverige."},
+  {"kpi": "U07443", "title":"U. Responstid (tid från 112-samtal till första resurs är på plats) för ambulans, mediantid i minuter", "description":"Uppgifter om responstider kommer från SOS Alarm. Med responstid avses tiden från det att larmcentralen får larmet till dess att räddningstjänsten anländer till skadeplatsen. Observera att endast insatser med syfte att rädda liv, egendom och miljö ingår. Mätperioden avser juni T-1 till juni T där värdet ligger på år T. Indikatorn är ett medianvärde i minuter. källa: SOS alarm"},
+  {"kpi": "U07415", "title":"U. Utvecklade bränder i byggnad, antal/1000 inv", "description":"Uppgifterna har hämtats från Räddningsverkets insatsregister som bygger på de insatsrapporter som räddningstjänsterna utformar efter insatser/utryckningar. Registret innehåller bland annat alla insatser som den kommunala räddningstjänsten gjort till brand i byggnad. Med byggnad menas såväl bostäder som allmänna byggnader, industribyggnader mm. Med utvecklad brand avses sådana insatser där det fortfarande brinner när räddningstjänsten anländer. Räddningsverkets insatsregister fångar enbart sådana bränder som varit föremål för räddningsinsats. Antalet räddningsinsatser har sedan justerats med uppgifter från SCB om befolkningen i respektive kommun. Uppgifterna gäller för perioden år T-4 till år T. Källa: MSB"},
+  {"kpi": "N00401", "title":"Utsläpp av växthusgaser inom kommunens gränser, ton CO2-ekv/inv", "description":"Utsläpp av växthusgaser inom kommunens geografiska gränser, ton CO2-ekvivalenter/invånare. Ibland kan enskilda industriverksamheter leda till höga utsläpp inom en kommuns gränser. Källa: RUS (Regional utveckling och samverkan i miljömålssystemet) samt Nationella emissionsdatabasen."},
+  {"kpi": "N05401", "title":"Valdeltagande i senaste kommunvalet, (%)", "description":"Antal avgivna röster i senaste kommunalvalet (giltiga och ogiltiga) dividerat med antal röstberättigade, multiplicerat med 100. Källa: Valmyndigheten och SCB."},
+  {"kpi": "N07400", "title":"Andel miljöbilar av totalt antal bilar i hela den geografiska kommunen, (%)", "description":"Antal miljöbilar i trafik dividerat med totalt antal bilar i trafik, multiplicerat med 100. Avser den 31/12 och hela den geografiska kommunen. Avser bilar som vid tiden för registrering uppfyllde kraven för miljöbil. Detta innebär att bilar registrerade före den 1 januari 2013 ska uppfylla kriterierna för MB2007 och bilar registrerade 1 januari 2013 eller senare ska uppfylla kriterierna för MB2013. Källa: SCB."},          
+  {"kpi": "N00402", "title":"Antibiotikaförsäljning kommun, recept/1000 inv", "description":"Avser läkemedelsartikelar med ATC-kod J01. J01XX05 (metenamin) är exkluderat. Gäller försäljning på apotek mot recept. Veterinär förskrivning och försäljning av dosdispenserade läkemedel är exkluderade. (däremot ingår hela sålda förpackningar även om receptet var ett dosrecept). Antal invånare är hämtade från SCB och avser den 31/12 år T-1. Källa: Apoteket Service AB."},
+  {"kpi": "N11102", "title":"Barn per årsarbetare i förskolan, antal", "description":"Antal barn, i åldern 1-5 år, i förskola totalt, dividerat med totalt antal årsarbetare i förskolan. Avser alla förskolor i kommunen oavsett regi. Uppgiften avser läsår, mätt 15/10. Källa: Skolverket."},
+  {"kpi": "N09803", "title":"Boklån kommunala bibliotek, antal/inv", "description":"Antal boklån dividerat med antal invånare 31/12. Källa: Kungliga biblioteket och SCB."},
+  {"kpi": "N00403", "title":"Ekologiskt odlad åkermark kommun, andel (%)", "description":"Ekologiskt odlad åkermark kommun, andel (%). Källa: Jordbruksverket."},          
+  {"kpi": "N07403", "title":"Anmälda våldsbrott i kommunen, antal/100 000 inv", "description":"Avser summering av: Dödligt våld, Försök till mord eller dråp, Misshandel inkl. grov, Våldtäkt inkl. grov, Grov kvinnofridskränkning, Grov fridskränkning, Olaga förföljelse, Våld mot tjänsteman och Rån inkl. grovt, antal/100 000 inv. Källa: BRÅ."},
+];   
+
+// Load Google Charts
+google.load("visualization", "1", {packages:["corechart"]});
+
+$(document).ready(function() {     
+  
+  // Add all the metrics to the page
+  $.each(metrics, function(index, metric){
+    $("#metrics_accordion").append('<div class="accordion-group">' + 
+                                   '   <div class="accordion-heading">' + 
+                                   '     <a class="accordion-toggle" data-toggle="collapse" data-parent="#metrics_accordion" href="#collapse'+index+'">' + 
+                                   '       '+metric.title+
+                                   '     </a>' + 
+                                   '   </div>' + 
+                                   '   <div id="collapse'+index+'" class="accordion-body collapse kolada-collapse" kpi="'+metric.kpi+'" title="'+metric.title+'">' + 
+                                   '     <div class="accordion-inner">' + 
+                                   '       <div id="chart_div_'+metric.kpi+'" style="height: 500px"></div>' +
+                                   '       <p id="description">'+metric.description+'</p>' + 
+                                   '       <p id="api_call">Kolada API-anrop: <a href="http://api.kolada.se/v1/data/permunicipality/'+metric.kpi+'/'+municipality+'">' +
+                                   '        http://api.kolada.se/v1/data/permunicipality/'+metric.kpi+'/'+municipality+'</a>' +
+                                   '         ('+metric.kpi+' = nyckeltalet, '+municipality+' = '+municipality_name+')</p>' +
+                                   '     </div>' + 
+                                   '   </div>' + 
+                                   '</div>');
+  }); //each            
+
+  // When a metric is expanded then call the Kolada API and display the graph of the metric
+  // Uses Bootstrap 3.0.0, if a later version of Bootstrap is used the event is not 'show.bs.collapse' but instead just 'show'
+  $('.kolada-collapse').on('show.bs.collapse', function () {
+      var kpi = $(this).attr("kpi");
+      var title = $(this).attr("title");
+      var chartData = new google.visualization.DataTable();
+      chartData.addColumn('string', 'År');
+      chartData.addColumn('number', title + ' (i ' + municipality_name + ')');
+                  
+      $.getJSON("http://api.kolada.se/v1/data/permunicipality/"+kpi+"/"+municipality, function(data) {                                    
+        var chartArray = [];
+        $.each(data.values, function(index, values) {
+          chartArray.push([values.period, values.value]);              
+        }); //each
+        chartData.addRows(chartArray);
+
+        var options = {
+          pointSize: 10 
+        };
+
+        // Begränsa antalet decimaler och använd komma som decimalsymbol
+        var formatter = new google.visualization.NumberFormat({decimalSymbol: ',', fractionDigits: 2});
+        formatter.format(chartData, 1);
+
+        var chart = new google.visualization.LineChart(document.getElementById('chart_div_'+kpi));
+        chart.draw(chartData, options);
+      }); //getJSON                                    
+    }); 
+
+});
